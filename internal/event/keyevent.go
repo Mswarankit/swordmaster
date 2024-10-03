9413094c8962cdd60ded7fb963a36f97c90b8a60
shrionit <sptiwari46289@gmail.com>
2024-10-03 12:57:06 +0530

    Created Keyboard Eventhandler

--- internal/event/keyevent.go
+++ internal/event/keyevent.go
@@ -1,1 +1,27 @@
 package event
+
+import (
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+type Keyboard struct {
+	listeners map[glfw.Key][]func()
+}
+
+func NewKeyboard() *Keyboard {
+	return &Keyboard{
+		listeners: make(map[glfw.Key][]func()),
+	}
+}
+
+func (keyboard *Keyboard) Listen(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	for _, listener := range keyboard.listeners[key] {
+		listener()
+	}
+}
+
+func (keyboard *Keyboard) AddListener(key glfw.Key, cb func()) {
+	listeners := keyboard.listeners[key]
+	listeners = append(listeners, cb)
+	keyboard.listeners[key] = listeners
+}
