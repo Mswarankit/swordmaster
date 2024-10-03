ed2c8a51fb6be789d1bd61963953ba918d48f806
shrionit <sptiwari46289@gmail.com>
2024-10-03 12:57:21 +0530

    Created Entity Renderer

---
+++ internal/renderer/entityrenderer.go
@@ -0,0 +1,30 @@
+package renderer
+
+import (
+	"swordmaster/internal/entity"
+	"swordmaster/pkg/window"
+
+	"github.com/tfriedel6/canvas"
+)
+
+type EntityRenderer struct {
+	entities []entity.Entity
+}
+
+func NewEntityRenderer(e ...entity.Entity) *EntityRenderer {
+	return &EntityRenderer{
+		entities: e,
+	}
+}
+
+func (r *EntityRenderer) Setup(w *window.Window) {
+	for _, e := range r.entities {
+		e.Setup(w)
+	}
+}
+
+func (r *EntityRenderer) Render(cv *canvas.Canvas, w, h float64) {
+	for _, e := range r.entities {
+		e.Draw(cv, w, h)
+	}
+}
