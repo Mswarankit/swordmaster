399502b41e8c76cb2c5ff2ef6e2d27b4701ff616
shrionit <sptiwari46289@gmail.com>
2024-10-03 12:58:15 +0530

    minor changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"swordmaster/internal/entity"
+	"swordmaster/internal/renderer"
+	"swordmaster/pkg/net"
 	wn "swordmaster/pkg/window"
-
-	"github.com/tfriedel6/canvas"
 )
 
 var window *wn.Window
 
 func main() {
-
 	window = wn.NewWindow(1366, 768, "MYGL")
-	window.Run(run)
-}
-
-func run(cv *canvas.Canvas, w, h float64) {
-	cv.SetFillStyle("#000")
-	cv.FillRect(0, 0, w, h)
-	cv.SetFillStyle("#00F")
-	cv.FillRect(w*0.25, h*0.25, w*0.5, h*0.5)
-	cv.SetStrokeStyle("#0F0")
-	cv.StrokeRect(window.MouseX-32, window.MouseY-32, 64, 64)
+	mrenderer := renderer.NewMasterRenderer(window)
+	mrenderer.Init([]renderer.Renderer{
+		renderer.NewEntityRenderer(
+			entity.NewPlayer(100, 100, 100),
+			entity.NewPlayer(100, 400, 100),
+		),
+	})
+	network := net.NewNetwork()
+	network.CreateServer()
+	defer network.Close()
+	window.Run(mrenderer.Render)
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 
 require (
 	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71
-	github.com/go-gl/glfw v0.0.0-20240506104042-037f3cc74f2a
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a
+	github.com/go-gl/mathgl v1.0.0
 	github.com/tfriedel6/canvas v0.12.1
 )
