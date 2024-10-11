9b9f6ce6fb8dfdb3543134bf5c92a8ac62b76bc9
mswarankit <ankitnightr14@gmail.com>
2024-10-11 19:15:36 +0530

    feat/bytechanges in network data

--- models/message.go
+++ models/message.go
@@ -1,7 +1,11 @@
 package models
 
+type PlayerState struct {
+	Position []float64 `json:"position"`
+}
+
 type Message struct {
-	Kind string    `json:"kind"`
-	Name string    `json:"name"`
-	Data []float64 `json:"data"`
+	Kind string      `json:"kind"`
+	Name string      `json:"name"`
+	Data PlayerState `json:"data"`
 }
---
+++ pkg/io/network.io.go
@@ -0,0 +1,21 @@
+package io
+
+import (
+	"encoding/json"
+	"log"
+)
+
+func ToBytes(data interface{}) []byte {
+	byteArray, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON to byte array: %v", err)
+	}
+	return byteArray
+}
+
+func FromBytes(data []byte, n interface{}) {
+	err := json.Unmarshal(data, n)
+	if err != nil {
+		log.Fatalf("Error marshalling Byte to JSON : %v", err)
+	}
+}
